f645eac35e4cac86868ec30561c972707c486649
miura <>
2018-10-12 12:00:13 +0900

    add pwa manifest etc.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,9 +1,19 @@
 <!doctype html>
 <html lang="{{ app()->getLocale() }}">
     <head>
+        <link rel="manifest" href="./manifest.json">
+        <script>
+        // service workerが有効なら、service-worker.js を登録します
+        if ('serviceWorker' in navigator) {
+            navigator.serviceWorker.register('./service-worker.js').then(function() { console.log('Service Worker Registered'); });
+        }
+        </script>
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="apple-mobile-web-app-status-bar-style" content="default">
 
         <link href="css/mmfuel-pwa.css" rel="stylesheet" type="text/css">
 
@@ -25,4 +35,10 @@
             <em class="fuelUnitText">km/l</em>
         </div>
     </body>
+    <script>
+    $(window).on('focus', function() {
+        console.log('window focused.');
+        location.reload();
+    });
+    </script>
 </html>
